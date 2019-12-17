e3ebffb8a0b8964bae0787cdb225ed9edc563898
mbjelac <marko.bjelac@softwaresauna.com>
2019-12-17 16:23:35 +0100

    Started main implementation.

--- src/path-follower.ts
+++ src/path-follower.ts
@@ -1,8 +1,15 @@
+import { AsciiMap, AsciiMapLocation } from './ascii-map/ascii-map';
+
 export interface CollectedLetters {
     letters: string;
     path: string;
 }
 
-export function followPath(map: string): CollectedLetters {
+export function followPath(mapString: string): CollectedLetters {
+
+    const map: AsciiMap = AsciiMap.fromString(mapString);
+
+    const start: AsciiMapLocation = map.find('@');
+
     throw Error('TODO!');
 }
