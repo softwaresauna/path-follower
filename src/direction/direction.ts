fda0e3d064c67bd8433bfb54fc3f281d286b6b78
mbjelac <marko.bjelac@softwaresauna.com>
2019-12-18 14:32:24 +0100

    Added readonly modifiers.

--- src/direction/direction.ts
+++ src/direction/direction.ts
@@ -3,10 +3,10 @@ import { AsciiMapLocation } from '../ascii-map/ascii-map';
 type Delta = -1 | 0 | 1;
 
 export class Direction {
-    static NORTH = new Direction(0, -1);
-    static SOUTH = new Direction(0, 1);
-    static WEST = new Direction(-1, 0);
-    static EAST = new Direction(1, 0);
+    static readonly NORTH = new Direction(0, -1);
+    static readonly SOUTH = new Direction(0, 1);
+    static readonly WEST = new Direction(-1, 0);
+    static readonly EAST = new Direction(1, 0);
 
     static getAll(): Direction[] {
         return [
