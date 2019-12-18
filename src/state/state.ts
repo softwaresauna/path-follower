0761d2a9b2173bd729d5c3783b0f1627558f35ac
mbjelac <marko.bjelac@softwaresauna.com>
2019-12-18 13:55:40 +0100

    Implemented collecting algorithm.

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,33 +1,44 @@
 import {
     AsciiMap,
     AsciiMapLocation,
-    FoundCharacter
+    FoundCharacter,
 } from '../ascii-map/ascii-map';
 import { Direction } from '../direction/direction';
 
 export interface CollectedLetters {
-    letters: string;
-    path: string;
+    readonly letters: string;
+    readonly path: string;
 }
 
 export class State {
     static from(
         map: AsciiMap,
-        startingAt: AsciiMapLocation
+        startingAt: AsciiMapLocation,
     ): State | undefined {
         return Direction.getAll()
-            .map(direction => new State(direction.goToNextLocation(startingAt)))
+            .map(
+                direction =>
+                    new State(map, direction.goToNextLocation(startingAt)),
+            )
             .find(state => notEmpty(map.getCharacterAt(state.location)));
     }
 
-    constructor(readonly location: AsciiMapLocation) {}
+    constructor(
+        private readonly map: AsciiMap,
+        readonly location: AsciiMapLocation,
+    ) {
+    }
 
     goToNextLocation(): State {
         throw new Error('Method not implemented.');
     }
 
     collect(soFar: CollectedLetters): CollectedLetters {
-        throw new Error('Method not implemented.');
+        const character = this.map.getCharacterAt(this.location);
+        return {
+            path: collectPath(character, soFar.path),
+            letters: collectLetter(character, soFar.letters),
+        };
     }
 
     isEndOfPath(): boolean {
@@ -37,4 +48,12 @@ export class State {
 
 export function notEmpty(character: FoundCharacter): boolean {
     return character !== undefined && character !== ' ';
+}
+
+export function collectLetter(character: FoundCharacter, oldLetters: string): string {
+    throw Error('TODO!');
+}
+
+export function collectPath(character: FoundCharacter, oldPath: string): string {
+    throw Error('TODO!');
 }
