50874a67fb3a93f2a52403156c7e393c3fa0d10d
mbjelac <marko.bjelac@softwaresauna.com>
2019-12-18 08:51:38 +0100

    Code improvement.

--- src/ascii-map/ascii-map.ts
+++ src/ascii-map/ascii-map.ts
@@ -13,7 +13,7 @@ export class AsciiMap {
     find(character: string): AsciiMapLocation {
         const foundLine = this.lines
             .map((line, index) => ({ line, y: index }))
-            .filter(indexedLine => indexedLine.line.includes(character))[0];
+            .find(indexedLine => indexedLine.line.includes(character));
 
         if (foundLine === undefined) {
             throw new Error(`Character ${character} not found!`);
