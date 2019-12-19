bc45144de26f2127924c38256aa07ef2e401e54d
mbjelac <marko.bjelac@softwaresauna.com>
2019-12-19 13:57:17 +0100

    Added manual-run program as usage example.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-throw Error('TODO!');
+import { AsciiMap } from './ascii-map/ascii-map';
+import { followPath } from './path-follower';
+
+const maps = [
+    `
+            @---A---+
+                    |
+            x-B-+   C
+                |   |
+                +---+
+            `,
+    `
+            @
+            | C----+
+            A |    |
+            +---B--+
+              |      x
+              |      |
+              +---D--+
+            `,
+    `
+              @---+
+                  B
+            K-----|--A
+            |     |  |
+            |  +--E  |
+            |  |     |
+            +--E--Ex C
+               |     |
+               +--F--+
+            `
+];
+
+maps.map(mapString => followPath(AsciiMap.fromString(mapString))).forEach(
+    collected =>
+        // tslint:disable-next-line:no-console
+        console.log('Letters: ', collected.letters, 'Path: ', collected.path)
+);
